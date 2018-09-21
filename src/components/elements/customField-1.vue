b5c1e5a4358c91f9fa9b09f3d0b92ed7447c3cd2
Carlos Loureda Parrado <balanzeneto@gmail.com>
2018-09-21 05:50:23 +0200

    feat: cutom field in progress yeah!
    - We can add a new field to a form, an extra attribute for that form model,
    keeps reactivity and we would need to be creative to order inputs but yeah it works
    perfect
    
    - Not possible to inject a new model with this method but it is a new model we
    just can add a new form bellow and that could do the trick

--- src/components/elements/customField-1.vue
+++ src/components/elements/customField-1.vue
@@ -5,13 +5,14 @@ Register thid field as a global components (main.js)
 <template>
     
         <input 
-        class="form-control" 
-        type="text" 
-        v-model="value" 
-        :disabled="disabled"
-        :maxlength="schema.max"
-        :placeholder="schema.placeholder"
-        :readonly="schema.readonly" >
+            class="form-control" 
+            type="text" 
+            @input="onInput"
+            v-model="value"
+            :disabled="disabled"
+            :maxlength="schema.max"
+            :placeholder="schema.placeholder"
+            :readonly="schema.readonly" >
     
 </template>
 
@@ -19,6 +20,64 @@ Register thid field as a global components (main.js)
    import { abstractField } from "vue-form-generator";
 
    export default {
-         mixins: [ abstractField ]
+         mixins: [ abstractField ],
+         methods: {
+            onInput($event) {
+                let value = $event.target.value;
+                console.log("value: ", value);
+                console.log("this.schema: ", this.schema);
+                /*switch(this.schema.type.toLowerCase()) {
+                    case "number":
+                    case "range":
+                        if(isNumber($event.target.valueAsNumber)) {
+                            value = $event.target.valueAsNumber;
+                        }
+                        break;
+                }*/
+                this.value = value;
+            },
+         }
    };
 </script>
+
+<!--
+<template lang="pug">
+.wrapper
+	input.form-control(
+		:id="getFieldID(schema)",
+		:type="schema.inputType.toLowerCase()",
+		:value="value",
+		@input="onInput",
+		@blur="onBlur",
+		:class="schema.fieldClasses",
+		@change="schema.onChange || null",
+		:disabled="disabled",
+		:accept="schema.accept",
+		:alt="schema.alt",
+		:autocomplete="schema.autocomplete",
+		:checked="schema.checked",
+		:dirname="schema.dirname",
+		:formaction="schema.formaction",
+		:formenctype="schema.formenctype",
+		:formmethod="schema.formmethod",
+		:formnovalidate="schema.formnovalidate",
+		:formtarget="schema.formtarget",
+		:height="schema.height",
+		:list="schema.list",
+		:max="schema.max",
+		:maxlength="schema.maxlength",
+		:min="schema.min",
+		:minlength="schema.minlength",
+		:multiple="schema.multiple",
+		:name="schema.inputName",
+		:pattern="schema.pattern",
+		:placeholder="schema.placeholder",
+		:readonly="schema.readonly",
+		:required="schema.required",
+		:size="schema.size",
+		:src="schema.src",
+		:step="schema.step",
+		:width="schema.width",
+		:files="schema.files")
+	span.helper(v-if="schema.inputType.toLowerCase() === 'color' || schema.inputType.toLowerCase() === 'range'") {{ value }}
+</template>-->
--- src/components/multiStepForm/multiStepForm.vue
+++ src/components/multiStepForm/multiStepForm.vue
@@ -11,19 +11,20 @@
         </div>
         <div>
             <h2>Venga! Pulsa el botón:</h2>
-            <button v-show="actualStep === 2" v-on:click.prevent="loadCutomFieldInSchema" type="button" class="btn btn-danger">No me presiones!👀</button>
+            <button v-show="actualStep === 2" v-on:click.prevent="loadCutomFieldInContactSchemaAndModel" type="button" class="btn btn-warning">No me presiones!👀</button>
+            <button v-show="actualStep === 2" v-on:click.prevent="loadCutomFieldInNewchemaAndModel" type="button" class="btn btn-danger">Este si que no ¡por Dios!😣</button>            
         </div>
         <div>
             <h2>El modelo se puede ver aquí en todo momento</h2>            
-            <pre>USER: {{models.userData.model}}</pre>
-            <pre>CONTACT: {{models.contactData.model}}</pre>
+            <pre v-bind="models.userData.model">USER: {{models.userData.model}}</pre>
+            <pre v-bind="models.contactData.model">CONTACT: {{models.contactData.model}}</pre>
             <pre>CARD: {{models.cardData.model}}</pre>
         </div>
     </div>  
 </template>
 
 <script>
-
+import Vue from "vue";
 import form1Gen from './form-1-generated';
 import form2Gen from './form-2-generated';
 import form3Gen from './form-3-generated';
@@ -43,6 +44,7 @@ export default {
         models: {
             userData, contactData, cardData
         },
+        fireUpdate: false,
         // selectedModel: userData,
         formOptions: {
             validateAfterLoad: true,
@@ -51,6 +53,9 @@ export default {
     } 
   },
   methods: {
+      updateView: function() {
+          this.fireUpdate = true
+      },
       // incr +1 or -1
     nextStep: function(incr){
         this.actualStep = this.actualStep <= 3 ? this.actualStep + incr : this.actualStep;       
@@ -58,40 +63,79 @@ export default {
     submit: () =>{
         alert("Ahora se enviaría el formulario!, pero podemos ver que el modelo está perfectamente actualizado y se mantiene reactivo todo el tiempo. Faltaría manejar prev-next con urls y eso")
     },
-    loadCutomFieldInSchema: function() {
-        var newModel = {
-            inputCustomField1: 'Dato cargado desde modelo dinámico ! 😎'
-        }
+    // Carga el customfield como un nuevo atributo y schema del modelo ContactData
+    loadCutomFieldInContactSchemaAndModel: function() {              
+            
         var dinamicNewSchema =  {
             fields: [
                 {
                     type: "custom1",
-                    label: "Custom Field (first try)",
-                    model: "inputCustomField1"
+                    label: "Etiqueta customizada (Schema dinámico en ContactData)",
+                    model: "inputCustomField1",
+                    placeholder: "Eaea"                   
                 },
             ]
-        };        
-        console.log(" this.contactData.schema.fields: ",  this.models.contactData.schema.fields)
-        console.log("dinamicNewSchema.fields[0]: ", dinamicNewSchema.fields[0])
-        //this.models.contactData.schema.fields.push(dinamicNewSchema.fields[0]) // o merge 
-        this.models.contactData.schema.fields = [
-            ...this.models.contactData.schema.fields, dinamicNewSchema.fields[0]
-        ]
-        this.models.contactData.model.inputCustomField1 = 'Dato cargado desde modelo dinámico ! 😎'
-        console.log(" this.contactData.schema.fields: ",  this.models.contactData.schema.fields)
-        //this.contactData.model
-    }
-  },
+        };      
+        /*let changeSchema  = true;        
+        if (this.models.contactData.schema.fields && this.models.contactData.schema.fields.length) {
+            this.models.contactData.schema.fields.map(el => {
+                if (el.type === "custom1") {
+                    changeSchema  = false;
+                }
+            })  
+        }*/
+        let msg = 'Dato cargado en modelo dinámico (extendido ContactData)! 😎';
+        this.models.contactData.model = Object.assign({}, this.models.contactData.model,{inputCustomField1: msg } );  
+        this.models.contactData.schema.fields.push(dinamicNewSchema.fields[0])
+        this.models.contactData.schema.fields = Object.assign({}, this.models.contactData.schema.fields)
+        //this.models.contactData.schema.fields = Object.assign({}, this.models.contactData.schema.fields, dinamicNewSchema.fields);  
+    },
+  //  Intenta cargar el customField como un modelo y un schema a parte
+   loadCutomFieldInNewchemaAndModel: function() {              
+        var newModel = {
+            inputCustomField1: 'Dato cargado desde modelo dinámico ! 😎'
+        }                
+        var dinamicNewSchema =  {
+            fields: [
+                {
+                    type: "custom1",
+                    label: "Etiqueta customizada (Schema nuevo)",
+                    model: "inputCustomField1",
+                    placeholder: "Eaea"                   
+                },
+            ]
+        };    
+
+        let newData = {
+            model: newModel,
+            schema: dinamicNewSchema
+        } 
+        this.models = Object.assign({}, this.models, {newModel: newData } );  
+        console.log("en la creacion: ", this.models)
+    },
+  },  
   computed: {
     selectedModel: function() {
-        let selected = userData;
+        console.log("Dynamic form fired creation")
+        let selected = this.models.userData;
         if (this.actualStep === 2) {
-            selected = contactData;
+            selected = this.models.contactData;
         } else if (this.actualStep === 3) {
-            selected = cardData;
+            selected = this.models.cardData;
         }
+        if(this.fireUpdate) {
+            selected = selected;
+            console.log("YUEYEYEYYEYEAIKSJBJ DSKJ SA")
+        }
+        console.log("selected: ", selected)
         return selected;
     }
+  },
+  created() {
+      console.log("Inside created")
+  },
+  beforeUpdate() {
+    console.log("Inside beforeUpdate")
   }
 }
 </script>
